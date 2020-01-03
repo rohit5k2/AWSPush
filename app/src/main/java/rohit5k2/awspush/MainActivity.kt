639b453ba4e4da02738b09debc0504bb0decfedb
Rohit Singh <rohit5k2@gmail.com>
2020-01-03 14:51:19 +0530

    project setup

---
+++ app/src/main/java/rohit5k2/awspush/MainActivity.kt
@@ -0,0 +1,13 @@
+
+package rohit5k2.awspush
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
